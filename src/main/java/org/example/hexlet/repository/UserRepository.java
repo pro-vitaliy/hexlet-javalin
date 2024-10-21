143925395221863bfd98b30560e874a6584e7b31
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-10-21 21:10:40 +0500

    feat: add UserRepository for handling user data

---
+++ src/main/java/org/example/hexlet/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package org.example.hexlet.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.Getter;
+import org.example.hexlet.model.User;
+
+public class UserRepository {
+    @Getter
+    private static List<User> entities = new ArrayList<>();
+
+    public static void save(User user) {
+        user.setId((long) entities.size() + 1);
+        entities.add(user);
+    }
+
+    public static Optional<User> find(Long id) {
+        return entities.stream()
+                .filter(entity -> entity.getId().equals(id))
+                .findAny();
+    }
+
+    public static List<User> search(String term) {
+        return entities.stream()
+                .filter(entity -> entity.getUsername().startsWith(term))
+                .toList();
+    }
+
+}
