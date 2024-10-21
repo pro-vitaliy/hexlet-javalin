4c51062403421acd01f7ab1aa5859d1565d6fd7e
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-10-21 20:58:38 +0500

    feat: move routes to separate class

---
+++ src/main/java/org/example/hexlet/NamedRoutes.java
@@ -0,0 +1,38 @@
+package org.example.hexlet;
+
+public class NamedRoutes {
+
+//    Users route
+    public static String usersPath() {
+        return "/users";
+    }
+
+    public static String buildUserPath() {
+        return "/users/build";
+    }
+
+    public static String userPath(Long id) {
+        return userPath(String.valueOf(id));
+    }
+
+    public static String userPath(String id) {
+        return "/users/" + id;
+    }
+
+//    Courses route
+    public static String coursesPath() {
+        return "/courses";
+    }
+
+    public static String buildCoursePath() {
+        return "/courses/build";
+    }
+
+    public static String coursePath(Long id) {
+        return coursePath(String.valueOf(id));
+    }
+
+    public static String coursePath(String id) {
+        return "/courses/" + id;
+    }
+}
