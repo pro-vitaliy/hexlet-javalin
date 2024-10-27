901f137cbfab28054166b3bba37c6870db8f0f2a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-10-27 19:54:38 +0500

    feat: add Main page

---
+++ src/main/java/org/example/hexlet/dto/MainPage.java
@@ -0,0 +1,10 @@
+package org.example.hexlet.dto;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+public class MainPage {
+    private String currentUser;
+}
