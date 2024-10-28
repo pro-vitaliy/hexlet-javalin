f272e7f74c5df858f378408f0a3d424c60723e69
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-10-28 23:05:04 +0500

    refactor: CoursesPage now inherits from BasePage

--- src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.example.hexlet.dto.BasePage;
 import org.example.hexlet.model.Course;
 
 @AllArgsConstructor
 @Getter
-public class CoursesPage {
+public class CoursesPage extends BasePage {
     private List<Course> courses;
     private String term;
 }
