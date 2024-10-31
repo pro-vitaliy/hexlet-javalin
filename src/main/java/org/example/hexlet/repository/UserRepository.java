31b26ef09b394b861329771a7763533d7e0600a2
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-10-31 23:29:44 +0500

    refactor: update UserRepository.save method to use H2 database

--- src/main/java/org/example/hexlet/repository/UserRepository.java
+++ src/main/java/org/example/hexlet/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.example.hexlet.repository;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -7,13 +9,25 @@ import java.util.Optional;
 import lombok.Getter;
 import org.example.hexlet.model.User;
 
-public class UserRepository {
+public class UserRepository extends BaseRepository {
     @Getter
     private static List<User> entities = new ArrayList<>();
 
-    public static void save(User user) {
-        user.setId((long) entities.size() + 1);
-        entities.add(user);
+    public static void save(User user) throws SQLException {
+        var sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try (var conn = dataSource.getConnection();
+                var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            var generatedKeys = stmt.getGeneratedKeys();
+            if(generatedKeys.next()) {
+                user.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
     }
 
     public static Optional<User> find(Long id) {
